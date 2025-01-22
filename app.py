409068f6bbfbbda918dc513a7cd32fac4b6b4869
Rimon Ramzy <rimonramzy023@gmail.com>
2025-01-22 13:46:06 +0200

    styling and functionality of signup page

--- app.py
+++ app.py
@@ -28,11 +28,23 @@ profile.config(bg="sky blue")
 signup_profile = Tk()
 signup_profile.withdraw()
 signup_profile.geometry("800x600")
+signup_profile.title("Sign Up")
+signup_profile.config(bg="sky blue")
+signup_profile.grid_columnconfigure(0, weight=1)
+signup_profile.grid_columnconfigure(1, weight=1)
+signup_profile.grid_columnconfigure(2, weight=1)
+signup_profile.grid_rowconfigure(0, weight=1)
+signup_profile.grid_rowconfigure(1, weight=1)
+signup_profile.grid_rowconfigure(2, weight=1)
+signup_profile.grid_rowconfigure(3, weight=1)
+signup_profile.grid_rowconfigure(4, weight=1)
+signup_profile.grid_rowconfigure(5, weight=1)
 
 
 
 def open_profile(data):
     root.withdraw()
+    signup_profile.withdraw()
 
     profile.deiconify()
     email_entry.delete(0, END)
@@ -54,50 +66,49 @@ def logout_fun() :
   root.deiconify()
 
 def signup_fun():
+  root.withdraw()
   signup_profile.deiconify()
   #Name
-  name_label_sign = Label(signup_profile, text="User Name:")
+  name_label_sign = Label(signup_profile, text="User Name:", font=("Roboto", 20), bg="sky blue")
   name_label_sign.grid(row=1, column=0)
   #Name Entry
-  name_frame_sign = Frame(signup_profile)
+  name_frame_sign = Frame(signup_profile, padx=5, pady=8, bg="lightblue")
   name_frame_sign.grid(row=1, column=1)
-  name_entry_sign = Entry(name_frame_sign, width=40)
+  name_entry_sign = Entry(name_frame_sign, width=40, font=80, bg="sky blue")
   name_entry_sign.pack()
   #Email
-  email_label_sign = Label(signup_profile, text="Email:")
+  email_label_sign = Label(signup_profile, text="Email:", font=("Roboto", 20), bg="sky blue")
   email_label_sign.grid(row=2, column=0)
   #Email Entry
-  email_frame_sign = Frame(signup_profile)
+  email_frame_sign = Frame(signup_profile, padx=5, pady=8, bg="lightblue")
   email_frame_sign.grid(row=2, column=1)
-  email_entry_sign = Entry(email_frame_sign, width=40)
+  email_entry_sign = Entry(email_frame_sign, width=40, font=80, bg="sky blue")
   email_entry_sign.pack()
   #Password
-  pass_label_sign = Label(signup_profile, text="Paaword:")
+  pass_label_sign = Label(signup_profile, text="Paaword:", font=("Roboto", 20), bg="sky blue")
   pass_label_sign.grid(row=3, column=0)
   #Password Entry
-  pass_frame_sign = Frame(signup_profile)
+  pass_frame_sign = Frame(signup_profile, padx=5, pady=8, bg="lightblue")
   pass_frame_sign.grid(row=3, column=1)
-  pass_entry_sign = Entry(pass_frame_sign, show="*", width=40)
+  pass_entry_sign = Entry(pass_frame_sign, show="*", width=40, font=80, bg="sky blue")
   pass_entry_sign.pack()
   #Signup Button
-  signup_btn = Button(signup_profile, text="Sign Up", command=login_fun)
-  signup_btn.grid(row=4, column=0, columnspan=3)
+  signup_btn = Button(signup_profile, text="Sign Up",font=("Roboto", 20), bg = "sky blue", command=lambda: login_fun(name_entry_sign.get()))
+  signup_btn.grid(row=4, column=0, columnspan=3, sticky="we" , padx= 50)
   #Go To Login
   login_frame = Frame(signup_profile)
   login_frame.grid(row=5, column=0, columnspan=3)
-  login_btn_go = Button(login_frame, text="Login", command=back_to_login)
-  login_label_go = Label(login_frame, text="If you have an account already, ")
+  login_btn_go = Button(login_frame, text="Login", font=("Roboto", 10), bg="sky blue", command=back_to_login)
+  login_label_go = Label(login_frame, text="If you have an account already, ", font=("Roboto", 10), bg="sky blue")
   login_label_go.pack(side="left")
   login_btn_go.pack(side="left")
+
   signup_profile.mainloop()
 
 
 def back_to_login() :
-  title['bg'] = "green2"
-
-
-def login_fun() :
-  title['bg'] = "brown3"
+  signup_profile.withdraw()
+  root.deiconify()
 
 
 
